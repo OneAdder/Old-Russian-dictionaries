838a3ea01f2362aab55d730c406225ce52a3612c
ansorox <adsorokina@edu.hse.ru>
2019-02-25 01:19:32 +0300

    modify searching and htmls of search results

--- old_rus_dictionaries.py
+++ old_rus_dictionaries.py
@@ -2,22 +2,34 @@ from flask import Flask, render_template, url_for, redirect, request
 import json
 
 with open('data.json', 'r', encoding='utf-8') as f:
-    js = json.load(f)
+    js = json.loads(f.read())
+    f.close()
 
 app = Flask(__name__)
 
 @app.route('/')
-def main_page():
+def index():
     return redirect(url_for('search'))
 
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     if request.method == 'POST':
-        query = request.form['word']
-        return str(js[query])
-    else:
-        return render_template('search.html')
+        qu = request.form['word']
+        return redirect(url_for('search_result', query=qu))
+    return render_template('search.html')
+
+@app.route('/res/<query>')
+def search_result(query):
+    try:
+        query = query.upper()
+        res = js[query]['data_XI–XVII']
+        # word = res[lexeme_XI–XVII]
+        # descr = res[Full]
+        return render_template('res.html', result=res)
+    except KeyError:
+        return render_template('error.html', comment='There is no such entry')
+
 
 if __name__ == '__main__':
     app.run(threaded=True)
