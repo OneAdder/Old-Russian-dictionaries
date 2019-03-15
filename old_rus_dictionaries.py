88bde1e24bbbd8485d76542c1b55d51e7cacbfee
ansorox <adsorokina@edu.hse.ru>
2019-03-15 17:46:30 +0300

    поиск по словарю Аванесова и симпатичный html с результатами поиска

--- old_rus_dictionaries.py
+++ old_rus_dictionaries.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, url_for, redirect, request
 import json
+from unification import unify
 
-with open('data.json', 'r', encoding='utf-8') as f:
+with open('avanesov.json', 'r', encoding='utf-8') as f:
     js = json.loads(f.read())
 
 app = Flask(__name__)
@@ -9,7 +10,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return redirect(url_for('search'))
-
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
@@ -20,25 +20,14 @@ def search():
 
 @app.route('/res/<query>')
 def search_result(query):
-    query = query.upper()
-    
-    mapping1 = {a: а, b: б}
-    mapping2 = {a: б, b: в}
-    query1 = ''
-    for letter in query:
-        query1 += mapping1[letter]
-    query2 = ''
-    for letter in query:
-        query2 += mapping2[letter]
-    
-    try:
-        res = js[query1]['data_XI–XVII']
-        return render_template('res.html', result=res)
-    except KeyError:
-        try:
-            res = js[query2]['data_XI–XVII']
-        except KeyError:
-            return render_template('error.html', comment='There is no such entry')
+    query = query.lower()
+    query = unify(query)
+
+    for entry in js:
+        if entry['unified_lemma'] == query:
+            return render_template('res.html', res=entry)
+
+    return render_template('error.html', comment='There is no such entry')
 
 
 if __name__ == '__main__':
