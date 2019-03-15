8e115eb0e80e5f6c612468f0b007740d222a1185
OneAdder <mikivo@list.ru>
2019-03-15 20:39:03 +0300

    пока тот выполнялся на одном ядре, я сделал распараллеливание на два. Так как одноядерный скрипт ещё не закончил, я не хочу его стопить. Теперь запустил двухядерный. В сумме они у меня покрыли три ядра. сижу на одном как дурак, жду

--- matcher.py
+++ matcher.py
@@ -1,7 +1,10 @@
 import json
 import pandas
-from unification import unify
+import itertools
 
+from multiprocessing import Pool
+
+from unification import unify
 
 with open('avanesov2.json', 'r', encoding='utf-8') as f:
     avanesov = json.loads(f.read())
@@ -11,37 +14,61 @@ shit = pandas.read_csv('wordlist_linked.csv', delimiter=',', header=0)
 x11 = list(shit.MainLemma)
 #print(x11[0:20])
 
-lengt = len(avanesov.keys())
+lengt = len(avanesov)
 print('Total:')
 print(lengt)
 
-bingos = 0
+
+def splitDict(d):
+    n = len(d) // 2
+    i = iter(d.items())
+
+    d1 = dict(itertools.islice(i, n))
+    d2 = dict(i)
+    return d1, d2
+
+avanesov1, avanesov2 = splitDict(avanesov)
+#print(len(avanesov1))
+#print(len(avanesov2))
+
 r = 0
-for avanesov_lemma in avanesov:
-    for x11_lemma in x11:
-        x11_unified = unify(x11_lemma)
-        if x11_unified == avanesov_lemma:
-            print()
-            print('Ready:')
-            print(100 - (((lengt - r) * 100)/lengt))
-            print(x11_unified)
-            i = x11.index(x11_lemma)
-            x11.pop(i)
-            avanesov[avanesov_lemma]['XVII_lemma'] = x11_lemma
-            avanesov[avanesov_lemma]['bingo'] = True
-            bingos += 1
-        else:
-            avanesov[avanesov_lemma]['bingo'] = False
-    r += 1
+bingos = 0
+
+def match_(avanesov):
+    global x11
+    global r
+    global bingos
+    for avanesov_lemma in avanesov:
+        for x11_lemma in x11:
+            x11_unified = unify(x11_lemma)
+            if x11_unified == avanesov_lemma:
+                print()
+                print('Ready:')
+                print(100 - (((lengt - r) * 100)/lengt))
+                print(x11_unified)
+                i = x11.index(x11_lemma)
+                x11.pop(i)
+                avanesov[avanesov_lemma]['XVII_lemma'] = x11_lemma
+                avanesov[avanesov_lemma]['bingo'] = True
+                bingos += 1
+            else:
+                avanesov[avanesov_lemma]['bingo'] = False
+        r += 1
+    return avanesov
+
+pool = Pool(processes=2)
+matched1 = pool.map(match_, (avanesov1,))
+matched2 = pool.map(match_, (avanesov2,))
+matched = {**matched1, **matched2}
 
 print('!bingos!')
 print(bingos)
 print('!bingos!')
 
 for x11_lemma in x11:
-    avanesov[avanesov_lemma]['XVII_lemma'] = x11_lemma
-    avanesov[avanesov_lemma]['bingo'] = False
+    matched[avanesov_lemma]['XVII_lemma'] = x11_lemma
+    matched[avanesov_lemma]['bingo'] = False
 
 with open('matched.json', 'w') as f:
-    json.dump(avanesov, f, indent=4, ensure_ascii=False)
+    json.dump(matched, f, indent=4, ensure_ascii=False)
 
