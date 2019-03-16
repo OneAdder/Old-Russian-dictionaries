35e592ef61b8181dfad0b4161079a39e751e3abe
OneAdder <mikivo@list.ru>
2019-03-16 16:26:20 +0300

    Скрипт, который запускает ВСЕ скрипты

--- data_processing/match.sh
+++ data_processing/match.sh
@@ -1,4 +1,6 @@
-echo 'Make some tea. This is gonna take some time (quadcore CPU: around one hour).'
+echo 'Parsing TEI'
+python3 tei_parser.py
+echo 'Ready. The longest part is starting. Make some tea. This is gonna take some time (quadcore CPU: around one hour).'
 python3 matcher.py
 echo 'The longest part is over.'
 python3 finish_match.py
